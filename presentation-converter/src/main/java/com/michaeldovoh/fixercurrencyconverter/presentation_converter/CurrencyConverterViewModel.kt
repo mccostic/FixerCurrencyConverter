020275b8fd6089e72f3e7bc9f0c9151120155156
Michael Dovoh <mccostic@gmail.com>
2023-01-12 18:13:17 +0000

    Added currencyviewmodel

---
+++ presentation-converter/src/main/java/com/michaeldovoh/fixercurrencyconverter/presentation_converter/CurrencyConverterViewModel.kt
@@ -0,0 +1,10 @@
+package com.michaeldovoh.fixercurrencyconverter.presentation_converter
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class CurrencyConverterViewModel @Inject constructor() : ViewModel() {
+
+}
