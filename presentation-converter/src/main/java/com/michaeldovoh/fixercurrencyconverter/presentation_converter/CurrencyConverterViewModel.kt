d5fa1a13c52f13b807f88723ac560a3cf8142d92
Michael Dovoh <mccostic@gmail.com>
2023-01-12 18:19:48 +0000

    implemented fetch currencies in viewmodel

--- presentation-converter/src/main/java/com/michaeldovoh/fixercurrencyconverter/presentation_converter/CurrencyConverterViewModel.kt
+++ presentation-converter/src/main/java/com/michaeldovoh/fixercurrencyconverter/presentation_converter/CurrencyConverterViewModel.kt
@@ -1,12 +1,37 @@
 package com.michaeldovoh.fixercurrencyconverter.presentation_converter
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.michaeldovoh.fixercurrencyconverter.domain.usecase.GetCurrenciesUseCase
+import com.michaeldovoh.fixercurrencyconverter.presentation_common.state.UiState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class CurrencyConverterViewModel @Inject constructor(private val useCase: GetCurrenciesUseCase,) : ViewModel() {
 
+    private val _currencyListFlow =
+        MutableStateFlow<UiState<CurrencyListModel>>(UiState.Loading)
+    val currencyListFlow: StateFlow<UiState<CurrencyListModel>> = _currencyListFlow
 
+    init {
+        loadCurrencies()
+    }
+    private fun loadCurrencies() {
+        viewModelScope.launch {
+            useCase.execute(GetCurrenciesUseCase.Request)
+                .map {
+
+
+                }
+                .collect {
+
+                }
+        }
+    }
 }
