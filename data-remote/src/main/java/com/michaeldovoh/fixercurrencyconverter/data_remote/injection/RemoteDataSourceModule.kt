9b60ad32e3cdf46fe72f4b151ce2afe4ac8c820e
Michael Dovoh <mccostic@gmail.com>
2023-01-12 17:18:25 +0000

    Added dependecy for RemoteDatasource

---
+++ data-remote/src/main/java/com/michaeldovoh/fixercurrencyconverter/data_remote/injection/RemoteDataSourceModule.kt
@@ -0,0 +1,17 @@
+package com.michaeldovoh.fixercurrencyconverter.data_remote.injection
+
+import com.michaeldovoh.fixercurrencyconverter.data_remote.source.RemoteCurrencyDataSourceImpl
+import com.michaeldovoh.fixercurrencyconverter.data_repository.data_source.remote.RemoteCurrencyDataSource
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RemoteDataSourceModule {
+
+    @Binds
+    abstract fun bindCurrencyDataSource(remoteCurrencyDataSourceImpl: RemoteCurrencyDataSourceImpl): RemoteCurrencyDataSource
+
+}
