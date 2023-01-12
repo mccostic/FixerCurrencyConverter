74a48147262921127bfb381f40bc8352a209263d
Michael Dovoh <mccostic@gmail.com>
2023-01-12 15:46:06 +0000

    Initial commit

---
+++ app/src/main/java/com/michaeldovoh/fixercurrencyconverter/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.michaeldovoh.fixercurrencyconverter
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.michaeldovoh.fixercurrencyconverter.ui.theme.FixerCurrencyConverterTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            FixerCurrencyConverterTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    FixerCurrencyConverterTheme {
+        Greeting("Android")
+    }
+}
