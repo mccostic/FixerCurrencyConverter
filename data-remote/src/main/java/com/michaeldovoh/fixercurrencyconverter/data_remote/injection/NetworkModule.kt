7d03d69b8094555ea5d9b22294ce1d1e2623debc
Michael Dovoh <mccostic@gmail.com>
2023-01-12 17:01:16 +0000

    fixed import issue with BuildConfig

--- data-remote/src/main/java/com/michaeldovoh/fixercurrencyconverter/data_remote/injection/NetworkModule.kt
+++ data-remote/src/main/java/com/michaeldovoh/fixercurrencyconverter/data_remote/injection/NetworkModule.kt
@@ -3,6 +3,7 @@ package com.michaeldovoh.fixercurrencyconverter.data_remote.injection
 
 import com.michaeldovoh.fixercurrencyconverter.data_remote.networking.currency.CurrencyService
 import com.squareup.moshi.Moshi
+import com.michaeldovoh.fixercurrencyconverter.data_remote.BuildConfig
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import dagger.Module
 import dagger.Provides
@@ -15,6 +16,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
+
 
 @Module
 @InstallIn(SingletonComponent::class)
