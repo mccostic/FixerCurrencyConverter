d97c3733c1ab3e2d741030cef99e94d4d7049f63
Michael Dovoh <mccostic@gmail.com>
2023-01-12 18:06:10 +0000

    added ConvertedScreen for converting result

---
+++ presentation-converter/src/main/java/com/michaeldovoh/fixercurrencyconverter/presentation_converter/ConverterStateScreen.kt
@@ -0,0 +1,111 @@
+package com.michaeldovoh.fixercurrencyconverter.presentation_converter
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import com.michaeldovoh.fixercurrencyconverter.presentation_common.state.UiState
+
+@Composable
+fun <T : Any> ConvertedScreen(trailingText:String,value:String,state: UiState<T>, onSuccess: @Composable (T) -> Unit) {
+    when (state) {
+        is UiState.Initial -> {
+            TextField(
+                value = value,
+                readOnly = true,
+                enabled = false,
+                onValueChange = { newValue -> {}},
+
+                singleLine = true,
+                trailingIcon = {
+                    Text(
+                        text = trailingText,
+                        color = MaterialTheme.colors.secondaryVariant,
+                        fontWeight = FontWeight.Bold
+                    )
+                },
+                shape = RoundedCornerShape(6.dp),
+                colors = TextFieldDefaults.textFieldColors(
+                    textColor = Color.DarkGray,
+                    backgroundColor = MaterialTheme.colors.secondary,
+                    disabledTextColor = Color.DarkGray,
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent,
+                    disabledIndicatorColor = Color.Transparent,
+                    cursorColor = MaterialTheme.colors.onSecondary
+                ),
+                //keyboard should only show numbers
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+            )
+        }
+        is UiState.Loading -> {
+            TextField(
+                value = value,
+                readOnly = true,
+                enabled = false,
+                onValueChange = { newValue -> {}},
+
+                singleLine = true,
+                trailingIcon = {
+                    Text(
+                        text = trailingText,
+                        color = MaterialTheme.colors.secondaryVariant,
+                        fontWeight = FontWeight.Bold
+                    )
+                },
+                shape = RoundedCornerShape(6.dp),
+                colors = TextFieldDefaults.textFieldColors(
+                    textColor = Color.DarkGray,
+                    backgroundColor = MaterialTheme.colors.secondary,
+                    disabledTextColor = Color.DarkGray,
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent,
+                    disabledIndicatorColor = Color.Transparent,
+                    cursorColor = MaterialTheme.colors.onSecondary
+                ),
+                //keyboard should only show numbers
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+            )
+        }
+        is UiState.Error -> {
+
+            TextField(
+                value = state.errorMessage,
+                readOnly = true,
+                enabled = false,
+                onValueChange = { newValue -> {}},
+
+                singleLine = true,
+                trailingIcon = {
+                    Text(
+                        text = trailingText,
+                        color = MaterialTheme.colors.secondaryVariant,
+                        fontWeight = FontWeight.Bold
+                    )
+                },
+                shape = RoundedCornerShape(6.dp),
+                colors = TextFieldDefaults.textFieldColors(
+                    textColor = Color.DarkGray,
+                    backgroundColor = MaterialTheme.colors.secondary,
+                    disabledTextColor = Color.DarkGray,
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent,
+                    disabledIndicatorColor = Color.Transparent,
+                    cursorColor = MaterialTheme.colors.onSecondary
+                ),
+                //keyboard should only show numbers
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+            )
+        }
+        is UiState.Success -> {
+            onSuccess(state.data)
+        }
+    }
+}
