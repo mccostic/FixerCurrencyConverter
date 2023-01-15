36c2f5969a03ab8e9d26bdacc20df61543c6adaa
Michael Dovoh <mccostic@gmail.com>
2023-01-15 12:49:14 +0000

    adde di for RateDataSource

--- data-remote/src/main/java/com/michaeldovoh/fixercurrencyconverter/data_remote/injection/RemoteDataSourceModule.kt
+++ data-remote/src/main/java/com/michaeldovoh/fixercurrencyconverter/data_remote/injection/RemoteDataSourceModule.kt
@@ -1,7 +1,9 @@
 package com.michaeldovoh.fixercurrencyconverter.data_remote.injection
 
 import com.michaeldovoh.fixercurrencyconverter.data_remote.source.RemoteCurrencyDataSourceImpl
+import com.michaeldovoh.fixercurrencyconverter.data_remote.source.RemoteRateDataSourceImpl
 import com.michaeldovoh.fixercurrencyconverter.data_repository.data_source.remote.RemoteCurrencyDataSource
+import com.michaeldovoh.fixercurrencyconverter.data_repository.data_source.remote.RemoteRateDataSource
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -15,4 +17,6 @@ abstract class RemoteDataSourceModule {
     abstract fun bindCurrencyDataSource(remoteCurrencyDataSourceImpl: RemoteCurrencyDataSourceImpl): RemoteCurrencyDataSource
 
 
+    @Binds
+    abstract fun bindRateDataSource(remoteRateDataSourceImpl: RemoteRateDataSourceImpl): RemoteRateDataSource
 }
