f44da52d0ecd8b964460c15d3795b5bd188d8984
Michael Dovoh <mccostic@gmail.com>
2023-01-15 13:34:04 +0000

    added di for HistoryRateRepository

--- data-repository/src/main/java/com/michaeldovoh/fixercurrencyconverter/data_repository/injection/RepositoryModule.kt
+++ data-repository/src/main/java/com/michaeldovoh/fixercurrencyconverter/data_repository/injection/RepositoryModule.kt
@@ -2,8 +2,10 @@ package com.michaeldovoh.fixercurrencyconverter.data_repository.injection
 
 
 import com.michaeldovoh.fixercurrencyconverter.data_repository.repository.CurrencyRepositoryImpl
+import com.michaeldovoh.fixercurrencyconverter.data_repository.repository.HistoryRateRepositoryImpl
 import com.michaeldovoh.fixercurrencyconverter.data_repository.repository.RateRepositoryImpl
 import com.michaeldovoh.fixercurrencyconverter.domain.repository.CurrencyRepository
+import com.michaeldovoh.fixercurrencyconverter.domain.repository.HistoryRateRepository
 import com.michaeldovoh.fixercurrencyconverter.domain.repository.RateRepository
 import dagger.Binds
 import dagger.Module
@@ -19,4 +21,8 @@ abstract class RepositoryModule {
 
     @Binds
     abstract fun bindRateRepository(rateRepositoryImpl: RateRepositoryImpl): RateRepository
+
+    @Binds
+    abstract fun bindHistoryRateRepository(historyRateRepositoryImpl: HistoryRateRepositoryImpl): HistoryRateRepository
+
 }
