73e02706a47dafd907dba196cfe6053da9f6ac08
Michael Dovoh <mccostic@gmail.com>
2023-01-15 15:57:15 +0000

    added test case for RemoteRateDataSourceImpl

---
+++ data-remote/src/test/java/com/michaeldovoh/fixercurrencyconverter/data_remote/RemoteRateDataSourceImplTest.kt
@@ -0,0 +1,33 @@
+package com.michaeldovoh.fixercurrencyconverter.data_remote
+
+import com.michaeldovoh.fixercurrencyconverter.data_remote.networking.currency.CurrencyApiModel
+import com.michaeldovoh.fixercurrencyconverter.data_remote.networking.currency.CurrencyService
+import com.michaeldovoh.fixercurrencyconverter.data_remote.networking.currency.RateApiModel
+import com.michaeldovoh.fixercurrencyconverter.data_remote.source.RemoteCurrencyDataSourceImpl
+import com.michaeldovoh.fixercurrencyconverter.data_remote.source.RemoteRateDataSourceImpl
+import com.michaeldovoh.fixercurrencyconverter.domain.entity.Currency
+import com.michaeldovoh.fixercurrencyconverter.domain.entity.Rate
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert
+import org.junit.Test
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.whenever
+
+class RemoteRateDataSourceImplTest {
+    private val currencyService = mock<CurrencyService>()
+    private val currencyDataSource = RemoteRateDataSourceImpl(currencyService)
+
+
+    @ExperimentalCoroutinesApi
+    @Test
+    fun testGetRate() = runTest {
+        val remoteRate = RateApiModel(rates = mapOf("GHS" to 10.0), success = true, date = "2023-01-12",
+            historical = true, timestamp = 1L, base = "USD")
+        val expectedRate = Rate(base = "USD", timestamp = 1L, target = "GHS", date = "2023-01-12", rate = 10.0)
+        whenever(currencyService.getRate(date = "2023-01-12", base = "USD", symbols = "GHS")).thenReturn(remoteRate)
+        val result = currencyDataSource.getRate(date = "2023-01-12", base = "USD", target = "GHS").first()
+        Assert.assertEquals(expectedRate, result)
+    }
+}
