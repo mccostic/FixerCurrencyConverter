69fabfaed73c8498e38051d2ceb09971043df65d
Michael Dovoh <mccostic@gmail.com>
2023-01-15 13:57:14 +0000

    added remeberSaveable for target and base currency

--- presentation-history/src/main/java/com/michaeldovoh/fixercurrencyconverter/presentation_history/HistoryScreen.kt
+++ presentation-history/src/main/java/com/michaeldovoh/fixercurrencyconverter/presentation_history/HistoryScreen.kt
@@ -1,9 +1,19 @@
 package com.michaeldovoh.fixercurrencyconverter.presentation_history
 
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.michaeldovoh.fixercurrencyconverter.presentation_common.navigation.HistoryInput
+
 
 @Composable
 fun HistoryScreen(
@@ -12,5 +22,12 @@ fun HistoryScreen(
     navController: NavController,
     input: HistoryInput
 ) {
+
+    val base by rememberSaveable {
+        mutableStateOf(input.baseCurrency)
+    }
+    val target by rememberSaveable {
+        mutableStateOf(input.targetCurrency)
+    }
 
 }
