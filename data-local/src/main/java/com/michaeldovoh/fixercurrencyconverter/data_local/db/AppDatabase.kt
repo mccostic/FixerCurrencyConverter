03e87aff524048e86159cee6d6cb27bcc459fa37
Michael Dovoh <mccostic@gmail.com>
2023-01-15 12:53:02 +0000

    added rate dao to database

--- data-local/src/main/java/com/michaeldovoh/fixercurrencyconverter/data_local/db/AppDatabase.kt
+++ data-local/src/main/java/com/michaeldovoh/fixercurrencyconverter/data_local/db/AppDatabase.kt
@@ -4,8 +4,12 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.michaeldovoh.fixercurrencyconverter.data_local.db.currency.CurrencyDao
 import com.michaeldovoh.fixercurrencyconverter.data_local.db.currency.CurrencyEntity
+import com.michaeldovoh.fixercurrencyconverter.data_local.db.rate.RateDao
+import com.michaeldovoh.fixercurrencyconverter.data_local.db.rate.RateEntity
 
-@Database(entities = [CurrencyEntity::class], version = 1)
+@Database(entities = [CurrencyEntity::class, RateEntity::class], version = 1)
 abstract class AppDatabase :RoomDatabase(){
     abstract fun currencyDao(): CurrencyDao
+    abstract fun rateDao(): RateDao
+
 }
