c58bb1c5a389aef9a552e6f2191c7586d6ab171a
Michael Dovoh <mccostic@gmail.com>
2023-01-15 13:32:54 +0000

    added usecase for get history

---
+++ domain/src/main/java/com/michaeldovoh/fixercurrencyconverter/domain/usecase/GetHistoryRateUseCase.kt
@@ -0,0 +1,31 @@
+package com.michaeldovoh.fixercurrencyconverter.domain.usecase
+
+import com.michaeldovoh.fixercurrencyconverter.domain.entity.HistoryRate
+import com.michaeldovoh.fixercurrencyconverter.domain.repository.HistoryRateRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+class GetHistoryRateUseCase @Inject constructor(
+    configuration: Configuration,
+    private val historyRepository: HistoryRateRepository,
+) : UseCase<GetHistoryRateUseCase.Request,
+        GetHistoryRateUseCase.Response>(configuration) {
+
+    override fun process(request:Request): Flow<Response> =
+        historyRepository.getHistoryRate(
+            base = request.base,
+            target = request.target,
+            startDate = request.startDate,
+            endDate = request.endDate)
+            .map {
+                Response(it)
+            }
+
+
+    data class Request(val base:String,val target:String,val startDate:String,val endDate:String) : UseCase.Request
+
+    data class Response(
+        val historyRates: List<HistoryRate>,
+    ) : UseCase.Response
+}
