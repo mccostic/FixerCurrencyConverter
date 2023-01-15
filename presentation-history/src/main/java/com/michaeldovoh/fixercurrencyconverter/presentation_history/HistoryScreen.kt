f04c45b52003d18eeedbad90eef220a75e211927
Michael Dovoh <mccostic@gmail.com>
2023-01-15 13:55:02 +0000

    added history rate screen

---
+++ presentation-history/src/main/java/com/michaeldovoh/fixercurrencyconverter/presentation_history/HistoryScreen.kt
@@ -0,0 +1,16 @@
+package com.michaeldovoh.fixercurrencyconverter.presentation_history
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import com.michaeldovoh.fixercurrencyconverter.presentation_common.navigation.HistoryInput
+
+@Composable
+fun HistoryScreen(
+    viewModel: HistoryViewModel,
+    modifier: Modifier,
+    navController: NavController,
+    input: HistoryInput
+) {
+
+}
