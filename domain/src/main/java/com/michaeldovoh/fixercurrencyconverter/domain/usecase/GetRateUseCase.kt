316d15260a9103903c605b4ef6fa368fd3516333
Michael Dovoh <mccostic@gmail.com>
2023-01-15 12:57:13 +0000

    added GetRateUseCase

---
+++ domain/src/main/java/com/michaeldovoh/fixercurrencyconverter/domain/usecase/GetRateUseCase.kt
@@ -0,0 +1,31 @@
+package com.michaeldovoh.fixercurrencyconverter.domain.usecase
+
+
+import android.util.Log
+import com.michaeldovoh.fixercurrencyconverter.domain.entity.Rate
+import com.michaeldovoh.fixercurrencyconverter.domain.repository.RateRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+class GetRateUseCase @Inject constructor(
+    configuration: Configuration,
+    private val rateRepository: RateRepository,
+) : UseCase<GetRateUseCase.Request,
+        GetRateUseCase.Response>(configuration) {
+
+    override fun process(request:Request): Flow<Response> =
+        rateRepository.getRate(base = request.base, target = request.target, date = request.date)
+            .map {
+                list->
+                Log.d("GetRateUseCase", list.toString())
+                Response(list)
+            }
+
+
+    data class Request(val base:String,val target:String,val date:String) : UseCase.Request
+
+    data class Response(
+        val rate: List<Rate> = listOf(),
+    ) : UseCase.Response
+}
