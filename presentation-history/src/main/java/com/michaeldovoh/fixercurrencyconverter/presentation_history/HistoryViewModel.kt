58409c4314f70e1eb2624300ee595d947a8e6d87
Michael Dovoh <mccostic@gmail.com>
2023-01-15 13:50:04 +0000

    added annotation for HiltViewModel

--- presentation-history/src/main/java/com/michaeldovoh/fixercurrencyconverter/presentation_history/HistoryViewModel.kt
+++ presentation-history/src/main/java/com/michaeldovoh/fixercurrencyconverter/presentation_history/HistoryViewModel.kt
@@ -1,4 +1,14 @@
 package com.michaeldovoh.fixercurrencyconverter.presentation_history
 
-class HistoryViewModel {
+import androidx.lifecycle.ViewModel
+import com.michaeldovoh.fixercurrencyconverter.domain.usecase.GetHistoryRateUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class HistoryViewModel @Inject constructor(private val useCase: GetHistoryRateUseCase,
+                        private val historyRateListConverter: HistoryRateListConverter,)
+    :ViewModel(){
+
+
 }
